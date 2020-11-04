afb8df0f04794e7d459e555b3797877d9d220cc3
Ray-Al-Amin <alraiman2k15@hotmail.com>
2020-11-04 21:24:50 +0100

    Add project files.

---
+++ Frage12/WhatAreYouDoingStepBro.feature
@@ -0,0 +1,21 @@
+﻿Feature: WhatAreYouDoingStepBro
+	Stuff
+
+@mytag
+Scenario: Sinus
+	Given the number is 90
+	And the settings are set to degrees
+	When the sinus is taken from this number
+	Then the result should be 1
+
+Scenario: Cosinus
+	Given the number is 90
+	And the settings are set to degrees
+	When the cosinus is taken from this number
+	Then the result should be 0
+
+Scenario: Tangens
+	Given the number is 180
+	And the settings are set to degrees
+	When the tangens is taken from this number
+	Then the result should be 0
---
+++ Frage12/WhatAreYouDoingStepBroSteps.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Frage12
+{
+    [Binding]
+    public class WhatAreYouDoingStepBroSteps
+    {
+        double number, number2;
+        [Given(@"the number is (.*)")]
+        public void GivenTheNumberIs(double p0)
+        {
+            number = p0;
+        }
+
+        [Given(@"the settings are set to degrees")]
+        public void GivenTheSettingsAreSetToDegrees()
+        {
+            // die funktionen rechnen nur in radian deswegen wird hier die degree zahl zu radian umgewandelt
+            number = number * Math.PI / 180;
+        }
+
+
+        [When(@"the sinus is taken from this number")]
+        public void WhenTheSinusIsTakenFrom()
+        {
+            number2 = Math.Sin(number);
+        }
+
+        [When(@"the cosinus is taken from this number")]
+        public void WhenTheCosinusIsTakenFrom()
+        {
+            number2 = Math.Cos(number);
+        }
+
+        [When(@"the tangens is taken from this number")]
+        public void WhenTheTangensIsTakenFrom()
+        {
+            number2 = Math.Tan(number);
+        }
+
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(double p0)
+        {
+            Assert.Equal(p0, number2, 5);
+        }
+    }
+}
